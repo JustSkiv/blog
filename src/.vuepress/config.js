dca7a82c421981f354513d41d77c4778614fa85c
nikolay.tuzov <nikolay.tuzov@lamoda.ru>
2021-07-14 16:57:28 +0300

    add yandex-metrika counter

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -20,6 +20,14 @@ module.exports = {
     ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
     ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
     ['link', {rel: "shortcut icon", href: "/favicon.ico"}],
+
+    //yandex-metrika
+    ['script', {}, `
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(82930849, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });
+    `],
+    ['noscript', {}, `
+        <div><img src="https://mc.yandex.ru/watch/82930849" style="position:absolute; left:-9999px;" alt="" /></div>
+    `],
   ],
 
   // wordPerminute: {cn: 300, en: 160},
